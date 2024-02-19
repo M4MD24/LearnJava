558636ef9e374c9dbd02da757b2e9e60e7527a3d
M3MD69 <shadowsorrow25900@gmail.com>
2024-02-19 10:33:21 +0200

    done 3 Edit 1

--- src/_1_learning/_1_7_data_structures_and_algorithms/_1_7_4_queue/Queue.java
+++ src/_1_learning/_1_7_data_structures_and_algorithms/_1_7_4_queue/Queue.java
@@ -1,21 +1,24 @@
 package _1_learning._1_7_data_structures_and_algorithms._1_7_4_queue;
 
 public class Queue<Type> {
-    private static int size = 0;
+    private static int size = 0,
+            rear = 0;
     private Type[] queue = (Type[]) new Object[size];
 
     public void enQueue(Type data) {
-        ++size;
+        if (size == rear) ++size;
         Type[] newQueue = (Type[]) new Object[getSize()];
-        newQueue[getSize() - 1] = data;
+        newQueue[rear] = data;
         System.arraycopy(queue, 0, newQueue, 0, getSize() - 1);
         queue = newQueue;
+        rear++;
     }
 
     public void deQueue() {
         Type[] newQueue = (Type[]) new Object[getSize()];
         System.arraycopy(queue, 1, newQueue, 0, getSize() - 1);
         queue = newQueue;
+        --rear;
     }
 
     public void showAll() {
