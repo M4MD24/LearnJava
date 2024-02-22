cd420420b5303a99376023162d46b9137e904708
M3MD69 <shadowsorrow25900@gmail.com>
2024-02-22 14:48:48 +0200

    1 - edit binary tree

--- src/_1_learning/_1_7_data_structures_and_algorithms/_1_7_6_tree/_1_7_6_1_binary_tree/Main.java
+++ src/_1_learning/_1_7_data_structures_and_algorithms/_1_7_6_tree/_1_7_6_1_binary_tree/Main.java
@@ -1,18 +1,33 @@
 package _1_learning._1_7_data_structures_and_algorithms._1_7_6_tree._1_7_6_1_binary_tree;
 
 public class Main {
+        private static final BinaryTree<Character> BINARY_TREE = new BinaryTree<>();
     public static void main(String[] args) {
-        BinaryTree<Character> binaryTree = new BinaryTree<>();
-        binaryTree.insert('a');
-        binaryTree.insert('d');
-        binaryTree.insert('b');
-        binaryTree.insert('e');
-        binaryTree.insert('c');
-        binaryTree.insert('f');
-        binaryTree.insert('z');
-        binaryTree.inorder();
+        insertTest();
+        inorderTest();
+        preorderTest();
+    }
+
+    private static void preorderTest() {
+        System.out.print("■ Preorder: ");
+        BINARY_TREE.preorder();
         System.out.println();
-        binaryTree.preorder();
+    }
+
+    private static void inorderTest() {
+        System.out.print("■ Inorder : ");
+        BINARY_TREE.inorder();
+        System.out.println();
+    }
+
+    private static void insertTest() {
+        BINARY_TREE.insert('A');
+        BINARY_TREE.insert('D');
+        BINARY_TREE.insert('B');
+        BINARY_TREE.insert('E');
+        BINARY_TREE.insert('C');
+        BINARY_TREE.insert('F');
+        BINARY_TREE.insert('Z');
     }
 }
 /*
