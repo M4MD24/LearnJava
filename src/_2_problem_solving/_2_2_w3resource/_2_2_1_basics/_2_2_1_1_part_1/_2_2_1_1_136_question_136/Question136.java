ede49b90fcbcae7440fe5aeed7e90c669e20d84a
M3MD69 <shadowsorrow25900@gmail.com>
2024-02-24 21:28:47 +0200

    done test question 136

--- src/_2_problem_solving/_2_2_w3resource/_2_2_1_basics/_2_2_1_1_part_1/_2_2_1_1_136_question_136/Question136.java
+++ src/_2_problem_solving/_2_2_w3resource/_2_2_1_basics/_2_2_1_1_part_1/_2_2_1_1_136_question_136/Question136.java
@@ -11,23 +11,26 @@ package _2_problem_solving._2_2_w3resource._2_2_1_basics._2_2_1_1_part_1._2_2_1_
  * <h2>Unique paths from top-left corner to bottom-right corner of the said grid: 3</h2>
  */
 public class Question136 {
-    public static int countUniquePaths(final int MAX_HEIGHT, final int MAX_WIDTH) {
-        if (MAX_HEIGHT <= 0 || MAX_WIDTH <= 0) return 0;
-        int[][] grid = new int[MAX_HEIGHT][MAX_WIDTH];
-        for (int height = MAX_HEIGHT - 1; height >= 0; --height)
-            for (int width = MAX_WIDTH - 1; width >= 0; --width)
-                grid[height][width] = getPaths(grid, height, width);
-        return grid[0][0];
-    }
-
-    private static int getPaths(final int[][] GRID, final int HEIGHT, final int WIDTH) {
-        if (HEIGHT >= GRID.length - 1 || WIDTH >= GRID[0].length - 1) return 1;
-        return GRID[HEIGHT][WIDTH + 1] + GRID[HEIGHT + 1][WIDTH];
-    }
-
     public static void main(String[] args) {
-        final int MAX_HEIGHT = 6,
-                MAX_WIDTH = 6;
+        final int MAX_HEIGHT = 0,
+                MAX_WIDTH = 0;
         System.out.println("Unique paths from top-left corner to bottom-right corner of the said grid: " + countUniquePaths(MAX_HEIGHT, MAX_WIDTH));
+    }
+
+    private static int countUniquePaths(int maxHeight, int maxWidth) {
+        if (maxHeight == 0 || maxWidth == 0) return 0;
+        else if (maxHeight == 1 || maxWidth == 1) return 1;
+        else {
+            --maxHeight;
+            --maxWidth;
+            return factorial((maxHeight - 1) + (maxWidth - 1)) / (factorial(maxHeight - 1) * factorial(maxWidth - 1));
+        }
+    }
+
+    private static int factorial(final int NUMBER) {
+        if (NUMBER == 0) return 0;
+        int product = 1;
+        for (int currentNumber = 1; currentNumber <= NUMBER; currentNumber++) product *= currentNumber;
+        return product;
     }
 }
