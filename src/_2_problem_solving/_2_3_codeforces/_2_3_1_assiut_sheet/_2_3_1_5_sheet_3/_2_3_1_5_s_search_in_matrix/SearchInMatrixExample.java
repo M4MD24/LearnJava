1e12fae2349b6915d3aa936977fa98042cb83351
مُحَمَّد سَعْدَاوِيْ <shadowsorrow25900@gmail.com>
2024-03-12 11:15:41 +0200

    ■ Edit readme.md file and Solve problem:
       ● Problem-Solving:
         ◘ Codeforces (+1)

---
+++ src/_2_problem_solving/_2_3_codeforces/_2_3_1_assiut_sheet/_2_3_1_5_sheet_3/_2_3_1_5_s_search_in_matrix/SearchInMatrixExample.java
@@ -0,0 +1,70 @@
+package _2_problem_solving._2_3_codeforces._2_3_1_assiut_sheet._2_3_1_5_sheet_3._2_3_1_5_s_search_in_matrix;
+
+import java.util.Scanner;
+
+/**
+ * <h1>S. Search in Matrix</h1>
+ * <h2>
+ * time limit per test: 2 second
+ * <br/>
+ * memory limit per test: 64 megabytes
+ * <br/>
+ * input: standard input
+ * <br/>
+ * output: standard output
+ * </h2>
+ * <br/>
+ * <h1>Question:</h1>
+ * <h2>Given two numbers N and M, a 2D array of size N * M and a number X. Determine whether X exists in the 2D array A or not.</h2>
+ * <br/>
+ * <h1>Input:</h1>
+ * <h2>
+ * 2 2
+ * <br/>
+ * 1 2
+ * <br/>
+ * 3 4
+ * <br/>
+ * 3
+ * </h2>
+ * <h1>Output:</h1>
+ * <h2>will not take number</h2>
+ * <br/>
+ * <h1>Input:</h1>
+ * <h2>
+ * 2 2
+ * <br/>
+ * 1 2
+ * <br/>
+ * 3 4
+ * <br/>
+ * 10
+ * </h2>
+ * <h1>Output:</h1>
+ * <h2>will take number</h2>
+ */
+
+public class SearchInMatrixExample {
+    private static final Scanner INPUT = new Scanner(System.in);
+    private static final byte COLUMNS = INPUT.nextByte(),
+            ROWS = INPUT.nextByte();
+    private static final int[][] NUMBERS = new int[COLUMNS][ROWS];
+
+    public static void main(String[] args) {
+        inputValues();
+        System.out.println(isContain(INPUT.nextInt()));
+    }
+
+    private static String isContain(final int TARGET_VALUE) {
+        for (byte column = 0; column < COLUMNS; column++)
+            for (byte row = 0; row < ROWS; row++)
+                if (NUMBERS[column][row] == TARGET_VALUE) return "will not take number";
+        return "will take number";
+    }
+
+    private static void inputValues() {
+        for (byte column = 0; column < COLUMNS; column++)
+            for (byte row = 0; row < ROWS; row++)
+                NUMBERS[column][row] = INPUT.nextInt();
+    }
+}
