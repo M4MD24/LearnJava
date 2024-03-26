0e7fdafe38da9623c581a826f83a62c392712b96
M3MD69 <shadowsorrow25900@gmail.com>
2024-03-26 12:29:15 +0200

    # Edit things and readme.md file and Solve problems:
      = Problem-Solving:
        - Codeforces (+5)

--- src/_2_problem_solving/_2_3_codeforces/_2_3_1_assiut_sheet/_2_3_1_10_sheet_7/_2_3_1_10_h_inverted_pyramid/InvertedPyramidExample.java
+++ src/_2_problem_solving/_2_3_codeforces/_2_3_1_assiut_sheet/_2_3_1_10_sheet_7/_2_3_1_10_h_inverted_pyramid/InvertedPyramidExample.java
@@ -1,9 +1,9 @@
-package _2_problem_solving._2_3_codeforces._2_3_1_assiut_sheet._2_3_1_10_sheet_7._2_3_1_10_g_pyramid;
+package _2_problem_solving._2_3_codeforces._2_3_1_assiut_sheet._2_3_1_10_sheet_7._2_3_1_10_h_inverted_pyramid;
 
 import java.util.Scanner;
 
 /**
- * <h1>G. Pyramid</h1>
+ * <h1>H. Inverted Pyramid</h1>
  * <h2>
  * time limit per test: 1 second
  * <br/>
@@ -15,21 +15,21 @@ import java.util.Scanner;
  * </h2>
  * <br/>
  * <h1>Question:</h1>
- * <h2>Given a number N. Print a pyramid of height N.</h2>
+ * <h2>Given a number N. Print an inverted pyramid of height N.</h2>
  * <br/>
  * <h1>Note:</h1>
  * <h2>Solve this problem using recursion.</h2>
  *
- * @see _2_problem_solving._2_3_codeforces._2_3_1_assiut_sheet._2_3_1_10_sheet_7._2_3_1_10_g_pyramid.images <h2>Examples and Note</h2>
+ * @see _2_problem_solving._2_3_codeforces._2_3_1_assiut_sheet._2_3_1_10_sheet_7._2_3_1_10_h_inverted_pyramid.images <h2>Examples and Note</h2>
  */
 
-public class PyramidExample {
+public class InvertedPyramidExample {
     private static final Scanner INPUT = new Scanner(System.in);
     private static short length = INPUT.nextShort();
     private static final String SPACE = " ",
             STAR = "*";
-    private static short starsCount = 1;
-    private static short spacesCount = (short) (length - starsCount);
+    private static short spacesCount = 0;
+    private static short starsCount = (short) (length * 2 - 1);
 
     public static void main(String[] args) {
         printPyramid();
@@ -38,8 +38,8 @@ public class PyramidExample {
     private static void printPyramid() {
         if (length-- > 0) {
             System.out.println(returnLine());
-            spacesCount--;
-            starsCount += 2;
+            spacesCount++;
+            starsCount -= 2;
             printPyramid();
         }
     }
