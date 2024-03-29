f7804c348a0b8c571127c9b3925f9120771946ae
M3MD69 <shadowsorrow25900@gmail.com>
2024-03-29 04:27:13 +0200

    # Change Intellij Idea to Android Studio and Update Java Version and Edit things and readme.md file and Solve problems:
      = Problem-Solving:
        - Codeforces (+2)

--- src/_1_learning/_1_1_basics/_1_1_10_keywords/_1_1_10_4_str/StrExample.java
+++ src/_1_learning/_1_1_basics/_1_1_10_keywords/_1_1_10_4_str/StrExample.java
@@ -3,6 +3,6 @@ package _1_learning._1_1_basics._1_1_10_keywords._1_1_10_4_str;
 public class StrExample {
     public static void main(String[] args) {
         final String NAME = "Mohamed";
-        System.out.print(STR."Hello \{NAME}");
+//        System.out.print(STR."Hello \{NAME}");
     }
 }
