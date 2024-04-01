0c97773c7daec450b0e9c36b5a1ee23db6ea5a0c
m3md69 <shadowsorrow25900@gmail.com>
2024-04-01 18:12:58 +0200

    # Edit things and readme.md file and Solve problems:
      = Problem-Solving:
        - Codeforces (+8)

--- src/_1_learning/_1_1_basics/_1_1_10_keywords/_1_1_10_4_str/StrExample.java
+++ src/_1_learning/_1_1_basics/_1_1_10_keywords/_1_1_10_4_str/StrExample.java
@@ -3,6 +3,6 @@ package _1_learning._1_1_basics._1_1_10_keywords._1_1_10_4_str;
 public class StrExample {
     public static void main(final String[] PARAMETERS) {
         final String NAME = "Mohamed";
-//        System.out.print(STR."Hello \{NAME}");
+        System.out.print(STR."Hello \{NAME}");
     }
 }
