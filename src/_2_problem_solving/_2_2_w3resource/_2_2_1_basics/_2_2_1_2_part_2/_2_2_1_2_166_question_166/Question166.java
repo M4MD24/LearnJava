c3cf51cd2523a6531513a78fcbaaf781903f7cbf
m3md69 <shadowsorrow25900@gmail.com>
2024-04-23 11:54:07 +0200

    ## Edit things and readme.md file
    ## Problem-Solving
    
    - W3Resource (+17)

---
+++ src/_2_problem_solving/_2_2_w3resource/_2_2_1_basics/_2_2_1_2_part_2/_2_2_1_2_166_question_166/Question166.java
@@ -0,0 +1,22 @@
+package _2_problem_solving._2_2_w3resource._2_2_1_basics._2_2_1_2_part_2._2_2_1_2_166_question_166;
+
+import java.util.Scanner;
+
+/**
+ * <h1>Question:</h1>
+ * <h2>Write a Java program to transform a given integer into String format.</h2>
+ * <br/>
+ * <h1>Input Data:</h1>
+ * <h2>Input an integer:  35</h2>
+ * <br/>
+ * <h1>Excepted Output:</h1>
+ * <h2>String format of the said integer: 35</h2>
+ */
+
+public class Question166 {
+    public static void main(final String[] ARGS) {
+        System.out.print("Input an integer: ");
+        final int NUMBER = new Scanner(System.in).nextInt();
+        System.out.println(STR."String format of the said integer: \{String.valueOf(NUMBER)}");
+    }
+}
---
+++ src/_2_problem_solving/_2_2_w3resource/_2_2_1_basics/_2_2_1_2_part_2/_2_2_1_2_173_question_173/Question173.java
@@ -0,0 +1,48 @@
+package _2_problem_solving._2_2_w3resource._2_2_1_basics._2_2_1_2_part_2._2_2_1_2_173_question_173;
+
+import java.util.Arrays;
+
+/**
+ * <h1>Question:</h1>
+ * <h2>Write a Java program to find the median of the numbers inside the window (size k) at each step in a given array of integers with duplicate numbers. Move the window to the array start.</h2>
+ * <br/>
+ * <h1>Excepted Output:</h1>
+ * <h2>
+ * Original array: [1, 2, 3, 4, 5, 6, 7, 8, 8]
+ * <br/>
+ * <br/>
+ * Value of k: 3
+ * <br/>
+ * <br/>
+ * Result:
+ * <br/>
+ * 2
+ * <br/>
+ * 3
+ * <br/>
+ * 4
+ * <br/>
+ * 5
+ * <br/>
+ * 6
+ * <br/>
+ * 7
+ * <br/>
+ * 8
+ * </h2>
+ */
+
+public class Question173 {
+    public static void main(final String[] ARGS) {
+        final int[] NUMBERS = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 8};
+        final int NUMBERS_REQUIRED_TO_BE_COMPILED = 3;
+        System.out.println(STR."Original array: \{Arrays.toString(NUMBERS)}\n");
+        System.out.println(STR."Value of k: \{NUMBERS_REQUIRED_TO_BE_COMPILED}\n");
+        mediansPrint(NUMBERS, NUMBERS_REQUIRED_TO_BE_COMPILED);
+    }
+
+    private static void mediansPrint(final int[] NUMBERS, final int NUMBERS_REQUIRED_TO_BE_COMPILED) {
+        for (int index = NUMBERS_REQUIRED_TO_BE_COMPILED / 2; index < NUMBERS.length - NUMBERS_REQUIRED_TO_BE_COMPILED / 2; index++)
+            System.out.println(NUMBERS[index]);
+    }
+}
