779b2803cea915f11c499eb77c163c4c9681d606
m3md69 <shadowsorrow25900@gmail.com>
2024-05-04 16:20:28 +0300

    # Edit and Solve problems
    ## Edit
    - things
    - readme.md file
    
    ## Problem-Solving
    - W3Resource (+1)

--- src/_2_problem_solving/_2_2_w3resource/_2_2_4_math_and_numbers/_2_2_4_1_math/_2_2_4_1_8_question_347/Question347.java
+++ src/_2_problem_solving/_2_2_w3resource/_2_2_4_math_and_numbers/_2_2_4_1_math/_2_2_4_1_8_question_347/Question347.java
@@ -1,43 +1,22 @@
 package _2_problem_solving._2_2_w3resource._2_2_4_math_and_numbers._2_2_4_1_math._2_2_4_1_8_question_347;
 
-import java.util.HashMap;
-
 /**
  * <h1>Question:</h1>
- * <h2>Write a Java program to convert a Roman number to an integer number.</h2>
+ * <h2>Write a Java program to convert an integer value to an absolute value.</h2>
  * <br/>
  * <h1>Excepted Output:</h1>
  * <h2>
- * Roman Number: DCCVII
+ * Input an integer number: -25
  * <br/>
- * Equivalent Number: 707
+ * The absolute value of -25 is: 25
  * </h2>
  */
 
 public class Question347 {
-    private static final HashMap<Character, Integer> ROMAN_NUMBERS = new HashMap<>();
-
-    static {
-        ROMAN_NUMBERS.put('I', 1);
-        ROMAN_NUMBERS.put('V', 5);
-        ROMAN_NUMBERS.put('X', 10);
-        ROMAN_NUMBERS.put('L', 50);
-        ROMAN_NUMBERS.put('C', 100);
-        ROMAN_NUMBERS.put('D', 500);
-        ROMAN_NUMBERS.put('M', 1000);
-    }
-
     public static void main(final String[] ARGS) {
-        final String ROMAN_CHARACTERS = "DCCVII";
+        final int NUMBER = -25;
         System.out.print(STR."""
-                Roman Numbers:\s\{ROMAN_CHARACTERS}
-                Equivalent:\s\{getEquivalentNumber(ROMAN_CHARACTERS)}""");
-    }
-
-    private static int getEquivalentNumber(final String ROMAN_CHARACTERS) {
-        int equivalentNumber = 0;
-        for (final char ROMAN_CHARACTER : ROMAN_CHARACTERS.toCharArray())
-            equivalentNumber += ROMAN_NUMBERS.get(ROMAN_CHARACTER);
-        return equivalentNumber;
+                Input an integer number:\s\{NUMBER}
+                The absolute value of \{NUMBER} is: \{NUMBER >= 0 ? NUMBER : -NUMBER}""");
     }
 }
