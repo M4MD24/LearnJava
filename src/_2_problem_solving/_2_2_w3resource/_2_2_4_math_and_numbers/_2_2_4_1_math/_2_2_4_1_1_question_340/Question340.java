aef2bf1b690e4707764a950038580dafad734ea2
m3md69 <shadowsorrow25900@gmail.com>
2024-05-04 15:37:53 +0300

    # Edit and Solve problems
    ## Edit
    - readme.md file
    
    ## Problem-Solving
    - W3Resource (+5)

---
+++ src/_2_problem_solving/_2_2_w3resource/_2_2_4_math_and_numbers/_2_2_4_1_math/_2_2_4_1_1_question_340/Question340.java
@@ -0,0 +1,17 @@
+package _2_problem_solving._2_2_w3resource._2_2_4_math_and_numbers._2_2_4_1_math._2_2_4_1_1_question_340;
+
+/**
+ * <h1>Question:</h1>
+ * <h2>Write a Java program to round up integer division results.</h2>
+ * <br/>
+ * <h1>Excepted Output:</h1>
+ * <h2>Percentage of Marks: 88%</h2>
+ */
+
+public class Question340 {
+    public static void main(final String[] ARGS) {
+        final int CURRENT_MARKS = 440,
+                TOTAL_MARKS = 500;
+        System.out.print(STR."Percentage of Marks: \{CURRENT_MARKS * 100 / TOTAL_MARKS}%");
+    }
+}
