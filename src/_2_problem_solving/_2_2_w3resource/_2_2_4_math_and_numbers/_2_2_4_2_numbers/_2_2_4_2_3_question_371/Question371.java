bc67c1e3a4ce66744031077f1a3caf2ab63c41e1
m3md69 <shadowsorrow25900@gmail.com>
2024-05-05 16:22:31 +0300

    # Edit and Solve problems
    ## Edit
    - readme.md file
    
    ## Problem-Solving
    - W3Resource (+2)

---
+++ src/_2_problem_solving/_2_2_w3resource/_2_2_4_math_and_numbers/_2_2_4_2_numbers/_2_2_4_2_3_question_371/Question371.java
@@ -0,0 +1,28 @@
+package _2_problem_solving._2_2_w3resource._2_2_4_math_and_numbers._2_2_4_2_numbers._2_2_4_2_3_question_371;
+
+import java.util.Random;
+
+/**
+ * <h1>Question:</h1>
+ * <h2>Write a Java program to generate random integers in a specific range.</h2>
+ * <br/>
+ * <h1>Excepted Output:</h1>
+ * <h2>
+ * Input the starting number of the range: 5
+ * <br/>
+ * Input the ending number of the range: 25
+ * <br/>
+ * 10
+ * </h2>
+ */
+
+public class Question371 {
+    public static void main(final String[] ARGS) {
+        final int STARTING_NUMBER_OF_RANGE = 5,
+                ENDING_NUMBER_OF_RANGE = 25;
+        System.out.print(STR."""
+                Input the starting number of the range\{STARTING_NUMBER_OF_RANGE}
+                Input the ending number of the range\{ENDING_NUMBER_OF_RANGE}
+                \{new Random().nextInt(STARTING_NUMBER_OF_RANGE, ENDING_NUMBER_OF_RANGE)}""");
+    }
+}
