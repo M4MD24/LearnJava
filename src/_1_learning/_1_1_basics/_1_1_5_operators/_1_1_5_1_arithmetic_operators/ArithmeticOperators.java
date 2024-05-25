b08982272cabd0401e888c945cc2eda6911e393d
m3md69 <shadowsorrow25900@gmail.com>
2024-05-25 21:22:49 +0300

    # Edit and Add Concepts
    ## Edit
    - things
    - readme.md file
    
    ## Add Concepts
    
    ### DSA (Data Structure and Algorithms)
    
    #### Binary Tree
    
    ##### Methods
    
    - Is Perfect
    - Clear All
    - Is Empty
    - Is Full
    - Get Size
    - Is Complete
    - Is Balanced
    - Is Degenerate
    - Get Height of Depth
    - Is Degenerate
    - Is Right Skewed
    - Is Left Skewed

--- src/_1_learning/_1_1_basics/_1_1_5_operators/_1_1_5_1_arithmetic_operators/ArithmeticOperators.java
+++ src/_1_learning/_1_1_basics/_1_1_5_operators/_1_1_5_1_arithmetic_operators/ArithmeticOperators.java
@@ -2,7 +2,7 @@ package _1_learning._1_1_basics._1_1_5_operators._1_1_5_1_arithmetic_operators;
 
 public class ArithmeticOperators {
     public static void main(final String[] ARGS) {
-        /* √ Similar Concepts
+        /* √
          * Prefix = Pre
          * Postfix = Post */
 
