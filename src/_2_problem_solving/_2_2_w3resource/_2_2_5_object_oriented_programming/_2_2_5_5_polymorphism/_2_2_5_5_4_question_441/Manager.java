8fece5052437d729145bf1a1a21adf6aae1219bf
m3md69 <shadowsorrow25900@gmail.com>
2024-06-05 21:51:39 +0300

    # Edit and Problem-Solving
    ## Edit
    - readme.md file
    
    ## Problem Solving
    
    ### W3Resource
    
    #### Object Oriented Programming
    
    - Encapsulation (+12)

---
+++ src/_2_problem_solving/_2_2_w3resource/_2_2_5_object_oriented_programming/_2_2_5_5_polymorphism/_2_2_5_5_4_question_441/Manager.java
@@ -0,0 +1,17 @@
+package _2_problem_solving._2_2_w3resource._2_2_5_object_oriented_programming._2_2_5_5_polymorphism._2_2_5_5_4_question_441;
+
+class Manager extends Employee {
+    private float baseSalary;
+    private final float BONUS;
+
+    Manager(final String NAME, final float BASE_SALARY, final float BONUS) {
+        super(NAME, "Manager");
+        this.baseSalary = BASE_SALARY;
+        this.BONUS = BONUS;
+    }
+
+    @Override
+    float calculateSalary() {
+        return baseSalary += BONUS;
+    }
+}
